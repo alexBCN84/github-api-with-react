0d1cc42c4cbbb717ec644572875d1e39209ed9cc
Alejandro Gines <alejandro.gines.martinez@zalando.de>
2018-06-02 08:02:11 +0200

    customising styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -58,8 +57,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <span className="gitHub-logo" alt="logo"><i class="fa fa-github-square"></i></span>
+          <h1 className="App-title">Welcome to GitHub profile finder</h1>
         </header>
         <form onSubmit={e => this.handleSubmit(e)}>
           <input ref="username" type="text" placeholder="username"/>
